d82fcd919bc25b4209e2cc0703efbc4948a6670c
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-21 00:22:03 -0600

    added space around to operators

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 (1..100).each do |i|
-    print "Fizz" if i%3==0 
-    print "Buzz" if i%5==0 
-    print i if i%3!=0 && i%5!=0
+    print "Fizz" if i%3 == 0 
+    print "Buzz" if i%5 == 0 
+    print i if i%3 != 0 && i%5 != 0
     print "\n"
 end
