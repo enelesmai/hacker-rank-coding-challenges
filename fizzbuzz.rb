ca2f73c153831478b6c1f9a71e80ab510433741d
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-21 00:21:01 -0600

    coding with less characters

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,6 @@
 (1..100).each do |i|
-    if i%15==0
-        puts "FizzBuzz"
-    elsif i%5==0
-        puts "Buzz"
-    elsif i%3==0
-        puts "Fizz"
-    else
-        puts i
-    end
+    print "Fizz" if i%3==0 
+    print "Buzz" if i%5==0 
+    print i if i%3!=0 && i%5!=0
+    print "\n"
 end
