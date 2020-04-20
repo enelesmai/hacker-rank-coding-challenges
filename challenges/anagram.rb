354c5f23d87df4df62b175b880ba8cb1df062fda
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-20 13:25:11 -0500

    applied rubocop to anagram

--- challenges/anagram.rb
+++ challenges/anagram.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # to be reviewed
 
 def anagram(s)
@@ -8,13 +9,18 @@ def anagram(s)
   string_one = s[0..length - 1]
   string_two = s[length..(s.length - 1)]
   string_one.split('').each do |char1|
+    next unless string_two.split('').any?(char1)
+
     found = -1
     string_two.split('').each_with_index do |char2, index|
-      found = index if char1 == char2
+      if char1 == char2
+        found = index
+        break
+      end
     end
-    string_two.slice!(found, 1) if found != -1
-  end
-  string_two.split('').count
+    string_two.slice!(found, 1)
+    end
+    string_two.split('').count
 end
 
 s = 'aaabbb'
