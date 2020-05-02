e31e64103b72a77eb351cb269caae2ff3778c09d
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-01 20:58:38 -0500

    updated anagram algorithm

--- challenges/anagram.rb
+++ challenges/anagram.rb
@@ -5,22 +5,19 @@
 def anagram(s)
   return -1 if s.length.odd?
 
-  length = s.length / 2
-  string_one = s[0..length - 1]
-  string_two = s[length..(s.length - 1)]
-  string_one.split('').each do |char1|
-    next unless string_two.split('').any?(char1)
+  half_length = s.length / 2
+  string_one = s[0..half_length - 1]
+  string_two = s[half_length..(s.length - 1)]
 
-    found = -1
-    string_two.split('').each_with_index do |char2, index|
-      if char1 == char2
-        found = index
-        break
-      end
-    end
-    string_two.slice!(found, 1)
-    end
-    string_two.split('').count
+  position = 0
+  while position < half_length
+    char1 = string_one[position]
+    found = string_two.index(char1)
+    string_two.slice!(found, 1) unless found.nil?
+    position += 1
+  end
+
+  string_two.length
 end
 
 s = 'aaabbb'
