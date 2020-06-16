62ac65bf9e2c06ae3f88e621b09d4de1d813a386
enelesmai <xochitl.selene.fp@gmail.com>
2020-06-15 22:35:10 -0500

    lists.rb

---
+++ challenges/lists.rb
@@ -0,0 +1,41 @@
+class Node
+    attr_accessor :value, :next_node
+    
+    def initialize(value, next_node = nil)
+        @value = value
+      @next_node = next_node
+    end
+  end
+  
+  class LinkedList
+    #setup head and tail
+    @tail = nil 
+    @head = nil
+    
+    def add(number)
+      # your code here
+      node = Node.new(number)
+      if @head == nil
+        @head = node
+        @tail = node
+      else
+        @tail.next_node = node
+        @tail = node
+      end
+    end
+  
+    def get(index)
+      current_node = @head
+      for i in 0..index-1 do
+        current_node = current_node.next_node
+      end
+      current_node.value
+    end
+  end
+  
+  list = LinkedList.new
+  
+  list.add(3)
+  list.add(5)
+  puts list.get(1)
+  # => 5
